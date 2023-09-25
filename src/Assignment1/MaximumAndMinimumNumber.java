5760447a54cc131880a7b3675215b046285168f9
“kirankatiyar” <kirankatiyar16@gmail.com>
2023-09-25 05:30:15 +0530

    Search maximum nd minimum element in array

--- src/Assignment1/MaximumAndMinimumNumber.java
+++ src/Assignment1/MaximumAndMinimumNumber.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class MaximumAndMinimumNumber {
     public static void findMax_minNumber(int[]a,int n) {
         int max = a[0] , min = a[0] ;
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i < n; i++) {
             if (max < a[i]) {
                 max = a[i];
             }
