4cbda46b049429a733affe138dda764b5b47613b
“kirankatiyar” <kirankatiyar16@gmail.com>
2023-09-26 17:32:49 +0530

    Search common element

--- src/Assignment1/SearchCommonElement.java
+++ src/Assignment1/SearchCommonElement.java
@@ -5,35 +5,37 @@ package Assignment1;
 import java.util.HashSet;
 import java.util.Scanner;
 public class SearchCommonElement {
-    public static void findCommonElement(int[] a,int[] b,int n){
-        HashSet<Integer> hst = new HashSet<>();
-        System.out.println("common elements are:");
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(a[i]==b[j]){
-                    hst.add(a[i]);
-                }
-            }
+    public static void findCommonElement(int[] a,int[] b){
+        // create hashsets
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+        // Adding elements from array a
+        for(int i : a ){
+            set1.add(i);
         }
-        for(int com_num :hst){
-            System.out.println(com_num+" ");
+        // Adding elements from array b
+        for(int j : b ){
+            set2.add(j);
         }
+        // use retainAll() method to find common elements
+        set1.retainAll(set2);
+        System.out.println("Common elements- " + set1);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the array range");
         int size = sc.nextInt();
-        int[] arr = new int[size];
-        int[] ary = new int[size];
+        int[] arr1 = new int[size];
+        int[] arr2 = new int[size];
         System.out.println("Enter the elements of list1::");
         for(int i=0; i<size; i++) {
-            arr[i] = sc.nextInt();
+            arr1[i] = sc.nextInt();
         }
         System.out.println("Enter the elements of list2::");
         for(int i=0; i<size; i++) {
-            ary[i] = sc.nextInt();
+            arr2[i] = sc.nextInt();
         }
 
-        findCommonElement(arr,ary,size);
+        findCommonElement(arr1,arr2);
     }
 }
